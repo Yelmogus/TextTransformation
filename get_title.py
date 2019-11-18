0be938870b3c3e73aa46817c4bbf38d826fabd09
Phacenox <omegathesigma@gmail.com>
2019-11-18 11:29:16 -0500

    Update get_title.py

--- get_title.py
+++ get_title.py
@@ -7,7 +7,7 @@
     page in the UI. Finds the first best, non-empty text tag, prioritizing <title>,
     <h1>, <h2>, <h3>, <h4>, <h5>, <h6>, then <p>, and returns a string
     of the first [n] characters. [n] is a constant that should be easily changeable.
-    If none non-empty tags are found, returns an empty string.
+    If no non-empty tags are found, returns an empty string.
     
 '''
 def get_title(doc)
