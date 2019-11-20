ab4e70193cf296ca8bfbe3f7cd9419d30d34fe96
Phacenox <omegathesigma@gmail.com>
2019-11-20 15:43:11 -0500

    Update get_title.py

--- get_title.py
+++ get_title.py
@@ -7,6 +7,7 @@
     page in the UI. Finds the first best, non-empty text tag, prioritizing <title>,
     <h1>, <h2>, <h3>, <h4>, <h5>, <h6>, then <p>, and returns a string
     of the first [n] characters. [n] is a constant that should be easily changeable.
+    Provide an option for infinite [n], when n=-1.
     If no non-empty tags are found, returns an empty string.
     
 '''
