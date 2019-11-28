3c28a0bac226d209a49a75997289934ce6379fdd
Yang Li <liy38@rpi.edu>
2019-11-28 03:30:41 -0500

    Added requirements.txt file

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ from flask import Flask
 from flask_json import FlaskJSON, JsonError, json_response, as_json
 from pattern.text.en import ngrams
 
+app = Flask(__name__)
 
-# app = Flask(__name__)
 
-
-# @app.route('/transform')
+@app.route('/transform')
 def handleInputs():
-    return 0
+    return "Hello World"
 
 
 if __name__ == '__main__':
-    print(ngrams("I am eating pizza", n=3))
-    #app.run()
+    app.run()
