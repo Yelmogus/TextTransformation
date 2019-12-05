79458269861633c9ad59b2d7295293e3a25d4140
krishg4 <krishg4@rpi.edu>
2019-12-05 11:19:02 -0500

    Added code to strip_input.py

--- src/strip_input.py
+++ src/strip_input.py
@@ -1,19 +1,30 @@
+from bs4 import BeautifulSoup
 '''
     stripInput:
-    params: String file_name
+    params: String doc
     return: String parsed_text
     errors:
-        - If the file name is invalid, throw InvalidFileException
+        - If the string is invalid, throw InvalidStringException
 
 
     description: Given a string filename, stripInput will:
-        1) Go through the file and remove all html tags/content (<title>, <h1>, <script> etc).
-        2) Go through the file and remove all extra characters (eg. ain''t -> ain't)
-        3) Go through the file and lowercase all words
-        4) Place the parse file into a new file parsed_text and return it
+        1) Go through the string and remove all html tags/content (<title>, <h1>, <script> etc).
+        2) Go through the string and remove all extra characters (eg. ain''t -> ain't)
+        3) Go through the string and lowercase all words
+        4) Remove all stop words () from the parsed text
+        4) Return the text
 
     ex: INPUT: "filename.txt"
     ex: OUTPUT: "parsedtext.txt"
 '''
-def stripInput(filename):
-    return None
+stop_words = []
+
+def stripInput(doc):
+    soup = BeautifulSoup(doc)
+    parsed_text = soup.get_text()
+    parsed_text.lower()
+
+    for x in stop_words:
+        parsed_text.replace(x, '')
+
+    return parsed_text
