bd7ee41e76b9c25fdc350a67d7e89020ef55b868
Yang Li <liy38@rpi.edu>
2019-12-07 14:31:22 -0500

    all the changes

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
 from flask import Flask, request, json
+from flask_json import FlaskJSON, JsonError, json_response, as_json
+from src import get_title, handle_input, location_ngrams, ngrams, strip_input
+import json
+from flask import Flask, request, json
 from flask_json import FlaskJSON, as_json
 from src import get_title, calculate_ngrams, strip_input
 import json
@@ -13,14 +17,14 @@ def handle_transformation():
     client_request_data = request.data.decode()
     data = json.loads(client_request_data)
 
-    stripped_text = strip_input.stripInput(data["data"])
+    stripped_text = strip_input.strip_input(data["data"])
     if data["transformations"]["title"]:
         title = get_title.get_title(data["data"])
     else:
         title = ""
 
     response = {"stripped": stripped_text,
-                "grams": calculate_ngrams.calculate_ngrams(stripped_text, data["transformations"]["grams"]),
+                "grams": location_ngrams.locationNGrams(stripped_text),
                 "title": title
                 }
     return response
