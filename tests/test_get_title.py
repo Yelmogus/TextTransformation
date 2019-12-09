1cbe5089002b214d915f235c2aaff4a847ff21de
phacenox <phacenox@gmail.com>
2019-12-09 11:43:35 -0500

    better test output for title

--- tests/test_get_title.py
+++ tests/test_get_title.py
@@ -10,6 +10,17 @@ import sys
 sys.path.insert(1, "../src")
 import get_title as g_t
 import math
+
+failed = 0
 for i in df.index:
-    print(str(df['Test Input'][i]))
-    print("o:'" + g_t.get_title(str(df['Test Input'][i])) + "'")
+    result = g_t.get_title(str(df['Test Input'][i]))
+    out = str(df['Expected Output'][i])
+    if out == 'nan':
+        out = ''
+    if out != result:
+        failed += 1
+        print("Test " + str(i) + " failed")
+        print(str(df['Test Input'][i]))
+        print("unexpected result:'" + result + "'")
+        print("expected:'" + str(df['Expected Output'][i]) + "'")
+print("Passed " + str(i-failed) + " tests out of " + str(i))
